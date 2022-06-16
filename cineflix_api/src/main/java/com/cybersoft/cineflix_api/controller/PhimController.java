4fe3afba98b9f8a92a9ae422f021648d1499ccaa
checongbinh20001990 <checongbinh20011990@gmail.com>
2022-06-16 18:36:55 +0700

    update thymeleaf

--- cineflix_api/src/main/java/com/cybersoft/cineflix_api/controller/PhimController.java
+++ cineflix_api/src/main/java/com/cybersoft/cineflix_api/controller/PhimController.java
@@ -1,11 +1,13 @@
 package com.cybersoft.cineflix_api.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +23,16 @@ public class PhimController {
 	
 	@GetMapping("")
 	public ResponseEntity<?> getAllPhim(){
-		List<Phim> listPhim = phimServiceImp.getAllPhim();
+		List<Map<String, ?>> listPhim = phimServiceImp.getAllPhim();
 		
-		return new ResponseEntity<List<Phim>>(listPhim, HttpStatus.OK);
+		return new ResponseEntity<List<Map<String, ?>>>(listPhim, HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id_quocgia}")
+	public ResponseEntity<?> getPhimByQuocGia(@PathVariable("id_quocgia") int id_quocgia){
+		List<Map<String, ?>> listPhim = phimServiceImp.getPhimByQuocGia(id_quocgia);
+		
+		return new ResponseEntity<List<Map<String, ?>>>(listPhim, HttpStatus.OK);
 	}
 	
 }
--- cineflix_api/src/main/java/com/cybersoft/cineflix_api/services/PhimServiceImp.java
+++ cineflix_api/src/main/java/com/cybersoft/cineflix_api/services/PhimServiceImp.java
@@ -1,9 +1,11 @@
 package com.cybersoft.cineflix_api.services;
 
 import java.util.List;
+import java.util.Map;
 
 import com.cybersoft.cineflix_api.entity.Phim;
 
 public interface PhimServiceImp {
-	public List<Phim> getAllPhim();
+	public List<Map<String, ?>> getAllPhim();
+	public List<Map<String, ?>> getPhimByQuocGia(Integer id);
 }
